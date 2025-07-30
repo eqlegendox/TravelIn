364403c5e3c9f061c3287b7c18acef9b8bf17a92
AA Gede Ngurah Ananda Wirasena <Ananda_W297@mitrais.com>
2025-07-30 08:50:12 +0800

    workstation migration

--- apps/web/app/(main)/(chatComponent)/chatToggler.tsx
+++ apps/web/app/(main)/(chatComponent)/chatToggler.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { Maximize, Minimize, Minus } from "lucide-react"
-import ChatPane from "./chatPane2"
+import ChatPane from "./chatPane"
 import { motion, AnimatePresence } from "framer-motion"
 import Image from "next/image"
 import Logo from "./logobos.svg"
@@ -27,6 +27,7 @@ export default function ToggleChat() {
         if (xpand) {
             return (
                 // Issue overflow is handled but static, any values below w40 couldn't be handled
+                <>
                 <AnimatePresence custom={-1000}>
                     <motion.div
                         className="fixed flex h-[100vh] w-[100vw] md:relative md:max-w-[40vw] "
@@ -35,7 +36,7 @@ export default function ToggleChat() {
                         animate = {{ x:0 }}
                         exit = {{ x:1000 }}
                         transition = {{ type: 'spring', stiffness: 100, damping: 16, duration: 0.1 }}
-                    >
+                        >
                         <div className="bg-primary fixed right-2 top-2 flex self-end p-1 rounded-full">
                             <Minimize className=" right-1 bg-background rounded-full p-1 m-1 w-5 h-5 hover:bg-red" onClick={toggleXpand} />
                             <Minus className=" right-1 bg-background rounded-full p-1 m-1 w-5 h-5 hover:bg-red" onClick= {toggleShow} />
@@ -45,11 +46,13 @@ export default function ToggleChat() {
                         </div>
                     </motion.div>
                 </AnimatePresence>
+                </>
             )
         }
         
         
         return (
+            <>
             <AnimatePresence custom={-1000}>
                 <motion.div 
                     className="parent flex flex-col float-right bottom-7 fixed h-[42vh] w-[100vw] md:w-[42vw] md:right-2 md:bottom-9 lg:w-[38vw] "
@@ -58,7 +61,7 @@ export default function ToggleChat() {
                     animate = {{ x: 0 }}
                     exit={{ x: 1000}}
                     transition = {{ type: 'spring', stiffness: 100, damping: 16, duration: 0.1 }}
-                >
+                    >
                     <div className="bg-primary flex self-end px-2 pl-6 rounded-tl-4xl rounded-tr-md">
                         <Maximize className=" right-1 bg-background rounded-full p-1 m-1 w-5 h-5 hover:bg-red" onClick={toggleXpand} />
                         <Minus className=" right-1 bg-background rounded-full p-1 m-1 w-5 h-5 hover:bg-red" onClick= {toggleShow} />
@@ -68,28 +71,31 @@ export default function ToggleChat() {
                     </div>
                 </motion.div>
             </AnimatePresence>
+            </>
         )
     }
 
     return (
+        <>
         <AnimatePresence custom={-1000}>
             <motion.div 
                 className="fixed right-4 bottom-4 p-1.5 bg-primary rounded-full aspect-square content-center"
-
+                
                 key="paneButton"
                 initial = {{ x: 1000}}
                 animate = {{ x: 0}}
                 transition={{ type: "spring", stiffness:100, damping: 16, duration: 0.1 }}
                 onClick={toggleShow}
-            >
+                >
                 <Image
                     priority
                     src={Logo}
                     alt=""
                     width={40}
-                />
+                    />
                 {/* <Lollipop onClick={toggleShow} className="text-white"/> */}
             </motion.div>
         </AnimatePresence>
+        </>
     )
 }
