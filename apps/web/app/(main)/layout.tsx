6de9bcf720fc131957afe77cf290808d5987c556
NandTheMan <156513107+NandTheMan@users.noreply.github.com>
2025-08-08 13:44:21 +0800

    ui improved and reviewed db-be implementation

--- apps/web/app/(main)/layout.tsx
+++ apps/web/app/(main)/layout.tsx
@@ -63,13 +63,15 @@ export default function RootLayout({
     }
   }, [currentUserId])
 
+  const placeholderino = "Laninaelnino"
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased flex w-vw`}
       >
         <Providers>{children}</Providers>
-        <div>{currentChatId? <ChatToggler currentChatId={currentChatId} currentUserId={currentUserId}/> : null}</div>
+        <div>{currentChatId? <ChatToggler currentChatId={currentChatId} currentUserId={currentUserId}/> : placeholderino }</div>
       </body>
     </html>
   )
