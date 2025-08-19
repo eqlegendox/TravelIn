e188df0e58666edc59a385d15e92652bc3f72056
NandTheMan <156513107+NandTheMan@users.noreply.github.com>
2025-08-19 10:16:05 +0800

    implemented real hotel query tools

--- apps/api/src/llm/graph/twools.ts
+++ apps/api/src/llm/graph/twools.ts
@@ -1,25 +1,47 @@
 const { tool } = require("@langchain/core/tools");
-import { CrawlerController } from "src/crawler/crawler.controller";
+// import { CrawlerController } from "src/crawler/crawler.controller";
 import { CrawlerService } from "src/crawler/crawler.service";
 // const { z } = require("zod");
 import * as z from "zod";
+import { ToolsService } from "src/tools/tools.service";
+import { PrismaService } from "src/prisma/prisma.service";
+import { Tool } from "@langchain/core/tools";
+
 const dbHotelSchema = z.object({
-  area: z.string(),
+  area: z.string().min(1).max(30),
+  minPrice: z.number().min(0).optional().transform(e => e === 0 ? undefined : e),
+  maxPrice: z.number().min(0).optional().transform(e => e === 0 ? undefined : e),
+  minRating: z.number().min(0).max(10).optional().transform(e => e === 0 ? undefined : e),
+  maxRating: z.number().min(0).max(10).optional().transform(e => e === 0 ? undefined : e),
+  minReview: z.number().min(0).optional().transform(e => e === 0 ? undefined : e),
+  maxReview: z.number().min(0).optional().transform(e => e === 0 ? undefined : e),
+  star: z.number().min(0).max(5).optional().transform(e => e === 0 ? undefined : e),
+  minStar: z.number().min(0).max(5).optional().transform(e => e === 0 ? undefined : e),
+  take: z.number().min(0).max(10).optional().transform(e => e === 0 ? undefined : e),
 });
 
 const dbHotelTool = tool(
   async (input) => {
-    const result = input.str + "wayaw";
-    return `Hotel Adi Permana ${input.area}, 1690 dollar a night, 5/5 reviews (4200 people);
-    Villa Kokonut, 20 dollar per hour, 4.6/5 reviews (123 people) `;
+    const hotelTool = new ToolsService(new PrismaService());
+
+    const query = hotelTool.orchestrateQuery(input)
+
+    const retrieved = await hotelTool.hotels({
+      skip: Math.floor(Math.random() * 5),
+      take: input.take || 3,
+      where: query
+    })
+
+    return JSON.stringify(await retrieved);
+    // return `Hotel Adi Permana ${input.area}, 1690 dollar a night, 5/5 reviews (4200 people);
+    // Villa Kokonut, 20 dollar per hour, 4.6/5 reviews (123 people) `;
   
   },
   {
     name: "hotelDatabaseQuery",
-    description: `You need to call this tool when user ask for hotel detail,
-    it accepts a general location in bali (please only pass one location, either the general or specific area) 
-    and will output some top hotel detail refering to the query.
-    after receiving this output please present it to the user as beautifully as possible`,
+    description: `You need to call this tool first when user ask for hotel detail,
+    it accepts a general location in bali (please only pass one location, either the general or specific area) and other already descriptive requirements
+    and will output some top hotel detail refering to the query.`,
     schema: dbHotelSchema,
   }
 );
@@ -68,10 +90,10 @@ const hotelCrawlerTool = tool(
   {
     name: "hotelWebCrawler",
     description: `You need to call this tool when user asks for hotel information that doesn't
-     exist in the hotelDatabase or when you want to search for hotel with specific requirements.
-     It accepts a general location in Bali (pass only one location, specific area or general area)
-     and hotel requirements. Date format is 'YYYY-MM-DD.`,
-     schema: crawlerParamaters
+      exist in the hotelDatabase or when you want to search for hotel with specific requirements.
+      It accepts a general location in Bali (pass only one location, specific area or general area)
+      and hotel requirements. Date format is 'YYYY-MM-DD.`,
+      schema: crawlerParamaters
   }
 )
 const TOOLS = [dbHotelTool, dbTourTool, hotelCrawlerTool]
--- apps/api/src/tools/tools.service.ts
+++ apps/api/src/tools/tools.service.ts
@@ -6,8 +6,10 @@ import { HotelInfo, Prisma } from 'database/generated/prisma';
 @Injectable()
 export class ToolsService {
     constructor(
-        private prisma: PrismaService,
-    ) {}
+        private readonly prisma: PrismaService,
+    ) {
+
+    }
 
     async hotels(params: {
         skip?: number;
@@ -28,27 +30,64 @@ export class ToolsService {
         return botels
     }
 
-    function orchestrateQuery (params: {
-        botelName: string,
-        minPrice: number,
-        maxPrice: number,
-        minRating: number,
-        maxRating: number, // don't know why, but still an option
-        minReview: number,
-        maxReview: number, // to look for hidden gem maybe?
+    public orchestrateQuery(params: {
+        area?: string,
+        minPrice?: number,
+        maxPrice?: number,
+        minRating?: number,
+        maxRating?: number, // don't know why, but still an option
+        minReview?: number,
+        maxReview?: number, // to look for hidden gem maybe?
+        star?: number, 
+        minStar?: number, 
     }): Prisma.HotelInfoWhereInput {
         const where: Prisma.HotelInfoWhereInput = {}
 
-        if (params.botelName) {
-            where.hotelName = {
-                contains: params.botelName,
+        if (params.area) {
+            where.location = {
+                contains: params.area,
                 mode: 'insensitive'
             };
         }
 
         if (params.minPrice !== null || params.maxPrice !== null) {
-            
+            where.price = {}
+            if (params.minPrice !== null) {
+                where.price.gte = params.minPrice;
+            } if (params.maxPrice !== null) {
+                where.price.lte = params.maxPrice;
+            }
         }
+        
+        if (params.minRating !== null || params.maxRating !== null) {
+            where.rating = {}
+            if (params.minRating !== null) {
+                where.rating.gte = params.minRating;
+            } if (params.maxRating !== null) {
+                where.rating.lte = params.maxRating;
+            }
+        }
+
+        if (params.minReview !== null || params.maxReview !== null) {
+            where.reviews = {}
+            if (params.minReview !== null) {
+                where.reviews.gte = params.minReview;
+            } if (params.maxReview !== null) {
+                where.reviews.lte = params.maxReview;
+            }
+        }
+
+        if (params.star !== null || params.minStar !== null) {
+            where.star = {}
+            if (params.star !== null) {
+                where.star = params.star;
+            } else if (params.minStar !== null) {
+                where.star.gte = params.minStar;
+            }
+        }
+            
+
+        return where;
     }
 
 }
